2cf097bf9549cfdde723c25ab9497ca10545f77e
weiwenzhe <394692092@qq.com>
2021-06-02 22:55:21 +0800

    增加stringbuffer的用法学习

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,14 @@
+import java.math.BigDecimal;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("heelaaoooooo");
+        System.out.println(sb.lastIndexOf("o"));
+        System.out.println(sb.indexOf("o"));
+        System.out.println(sb.replace(1,2,"hm"));
+
+
+
     }
 }

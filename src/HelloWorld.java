25beccf8228b2af6a54fe01059adb43f9743372a
weiwenzhe <394692092@qq.com>
2022-08-13 19:41:44 +0800

    1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.math.BigDecimal;
 
 public class HelloWorld {
@@ -7,6 +9,9 @@ public class HelloWorld {
         System.out.println(sb.lastIndexOf("o"));
         System.out.println(sb.indexOf("o"));
         System.out.println(sb.replace(1,2,"hm"));
+        System.out.println("my world");
+        //TODO 测试下todo
+        System.out.println("my world");
 
 
 
